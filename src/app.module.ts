e7f846178ff33f15dd3a239541f027c8a3d199a5
troy <532067334@qq.com>
2026-09-14 11:40:46 +0800

    fix: 修改demo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { DemoModule } from './demo/demo.module.js';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [DemoModule],
+  imports: [DemoModule, PrismaModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
